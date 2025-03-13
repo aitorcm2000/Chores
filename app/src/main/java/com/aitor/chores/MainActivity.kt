536d3613e24d7a94cb890a89844c1bf51be98a01
aitor <aitorcarrenoxd@gmail.com>
2025-03-13 09:27:04 +0100

    Añadido RCV_Deco_List, da estilo a los items de un rcv. Tweaks en menu_items Muestra el rcv bien

--- app/src/main/java/com/aitor/chores/MainActivity.kt
+++ app/src/main/java/com/aitor/chores/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rcv: RecyclerView
+    private val datos : ArrayList<DatosMenuItem> = arrayListOf(
+        DatosMenuItem("Menu1"),
+        DatosMenuItem("Menu2")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        rcv = findViewById(R.id.rcv_1)
+        rcv.addItemDecoration(RCV_Deco_List(20))
+        rcv.setLayoutManager(LinearLayoutManager(this))
+        val rcv1_adapter = MenuItemAdapter_Main(datos)
+        rcv.adapter = rcv1_adapter
     }
 }
--- app/src/main/java/com/aitor/chores/MenuItemAdapter_Main.kt
+++ app/src/main/java/com/aitor/chores/MenuItemAdapter_Main.kt
@@ -12,11 +12,24 @@ class MenuItemAdapter_Main  (private val datos: List<DatosMenuItem>):
 
     class ViewHolder(v: View):RecyclerView.ViewHolder(v){
         val titulo : TextView = v.findViewById(R.id.tv_titulo_menu)
-        val img : ImageView = v.findViewById(R.id.img_menu)
+        //val img : ImageView = v.findViewById(R.id.img_menu)
     }
 
     //Por cada elemento nuevo deberia cambiar entre derecha e izquierda
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v: View = LayoutInflater.from(parent.context).inflate(R.layout.)
+        val v: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.menu_item_der,parent,false)
+        val viewHolder = ViewHolder(v)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: MenuItemAdapter_Main.ViewHolder, position: Int) {
+        val titulo = datos[position].Titulo
+        //val img = datos[position].Imagen
+        holder.titulo.text = titulo
+    }
+
+    override fun getItemCount(): Int {
+        return datos.size
     }
 }
